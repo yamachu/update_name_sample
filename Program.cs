a563d87f7cc4a82747c960c5b00a3ebe655e01fb
Yusuke Yamada <yusuke.yamada.t1@dc.tohoku.ac.jp>
2016-12-26 15:55:26 +0900

    Add sample code - connect stream api

--- Program.cs
+++ Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using CoreTweet;
+using CoreTweet.Streaming;
 
 namespace ConsoleApplication
 {
+    public class TwitterBot
+    {
+        private Tokens MyToken;
+
+        public TwitterBot()
+        {
+            var CONSUMER_KEY = "Write Your Consumer Key";
+            var CONSUMER_SECRET = "Write Your Consumer Secret";
+            var OAUTH_ACCESS_TOKEN = "Write Your Access Token";
+            var OAUTH_ACCESS_SECRET = "Write Your Access Secret";
+
+            MyToken = Tokens.Create(CONSUMER_KEY, CONSUMER_SECRET,
+                                    OAUTH_ACCESS_TOKEN, OAUTH_ACCESS_SECRET);   
+        }
+
+        public void Run()
+        {
+            var stream = MyToken.Streaming.UserAsObservable();
+
+            stream.OfType<StatusMessage>()
+            .Subscribe(x => {
+                System.Console.WriteLine($"{x.Status.User.ScreenName}: {x.Status.Text}");
+            });
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start my Twitter Bot!");
+            var bot = new TwitterBot();
+            bot.Run();
+
+            Console.ReadKey(true);
         }
     }
 }
