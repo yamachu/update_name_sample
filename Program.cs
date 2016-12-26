0343042e85940e1dbb1f3320bf550cc4ebd39cf4
Yusuke Yamada <yusuke.yamada.t1@dc.tohoku.ac.jp>
2016-12-26 18:37:25 +0900

    Add minimum update_name

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Text.RegularExpressions;
 using CoreTweet;
 using CoreTweet.Streaming;
 
@@ -9,6 +10,7 @@ namespace ConsoleApplication
     public class TwitterBot
     {
         private Tokens MyToken;
+        private const string MyScreenName = "Your ScreenName => ex. y_chu5";
 
         public TwitterBot()
         {
@@ -18,7 +20,7 @@ namespace ConsoleApplication
             var OAUTH_ACCESS_SECRET = "Write Your Access Secret";
 
             MyToken = Tokens.Create(CONSUMER_KEY, CONSUMER_SECRET,
-                                    OAUTH_ACCESS_TOKEN, OAUTH_ACCESS_SECRET);   
+                                    OAUTH_ACCESS_TOKEN, OAUTH_ACCESS_SECRET);
         }
 
         public void Run()
@@ -26,9 +28,30 @@ namespace ConsoleApplication
             var stream = MyToken.Streaming.UserAsObservable();
 
             stream.OfType<StatusMessage>()
-            .Subscribe(x => {
-                System.Console.WriteLine($"{x.Status.User.ScreenName}: {x.Status.Text}");
-            });
+            .Where(x => x.Type == MessageType.Create)
+            .Select(x => x.Status)
+            .Subscribe(ParseTweet);
+        }
+
+        private async void ParseTweet(Status status)
+        {
+            var update_name_pattern = $@"^@{MyScreenName}\s+update_name\s+(?<next_name>.+)$";
+            var re = new Regex(update_name_pattern, RegexOptions.IgnoreCase);
+
+            var match = re.Match(status.Text);
+            if (!match.Success) return;
+
+            var next_name = match.Groups["next_name"].Value;
+            System.Console.WriteLine($"Try run update_name! => {status.User.ScreenName}: {status.Text}");
+
+            try
+            {
+                await MyToken.Account.UpdateProfileAsync(new {name = next_name});
+            }
+            catch(CoreTweet.TwitterException ex)
+            {
+                System.Console.WriteLine(ex);
+            }
         }
     }
 
